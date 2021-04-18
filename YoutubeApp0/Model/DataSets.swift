ffc8258c5f0fbbf5aa6063425070a70a2c666c29
github airy13-skyhi <airy13@icloud.com>
2021-04-18 18:18:23 +0900

    DataSetsModel追加

---
+++ YoutubeApp0/Model/DataSets.swift
@@ -0,0 +1,22 @@
+//
+//  DataSets.swift
+//  YoutubeApp0
+//
+//  Created by Manabu Kuramochi on 2021/04/18.
+//
+
+import Foundation
+
+struct DataSets {
+    
+    //プロパティ
+    let videoID:String?
+    let title:String?
+    let description:String?
+    let url:String?
+    let channelTitle:String?
+    let publishTime:String?
+    
+}
+
+
